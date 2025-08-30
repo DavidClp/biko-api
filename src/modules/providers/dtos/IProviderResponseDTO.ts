f4c2ff22cfb99ac23f651aabc462c765ee1918db
David Clipel <davidbalk999@gmail.com>
2025-08-29 21:37:42 -0400

    Atualiza IProviderResponseDTO para incluir novos campos: description, photoUrl, socialLinks e status.

--- src/modules/providers/dtos/IProviderResponseDTO.ts
+++ src/modules/providers/dtos/IProviderResponseDTO.ts
@@ -1,10 +1,16 @@
+import { ProviderStatus } from "@prisma/client";
+
 export interface IProviderResponseDTO {
   id: string;
   userId: string;
   name: string;
   service: string;
+  description?: string;
+  photoUrl?: string;
   city?: string;
   phone: string;
+  socialLinks?: string;
+  status: ProviderStatus;
   createdAt: Date;
   updatedAt: Date;
 }
