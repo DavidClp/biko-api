d839ed3a74e914ec9756ba977557e12a2a9a7371
David Clipel <davidbalk999@gmail.com>
2025-09-17 20:34:26 -0400

    feat: adiciona suporte para processamento e upload de imagens em formato WebP no caso de uso de atualização de imagem de perfil, utilizando a biblioteca sharp para otimização de imagens

--- src/modules/providers/useCases/UpdateImgProfileUseCase.ts
+++ src/modules/providers/useCases/UpdateImgProfileUseCase.ts
@@ -1,10 +1,11 @@
 import { IProviderRepository } from '../repositories';
-import { UpdateProviderDTO, ProviderResponseDTO } from '../dtos';
+import { ProviderResponseDTO } from '../dtos';
 import AppError from '../../../shared/errors/AppError';
 import { uploadToS3 } from '@/shared/helpers/uploadToS3';
+import sharp from 'sharp';
 
 export class UpdateImgProfileUseCase {
-  constructor(private providerRepository: IProviderRepository) {}
+  constructor(private providerRepository: IProviderRepository) { }
 
   async execute(id: string | undefined, file: Express.Multer.File): Promise<ProviderResponseDTO> {
     if (!id) {
@@ -26,13 +27,35 @@ export class UpdateImgProfileUseCase {
       });
     }
 
-    
-    const key = `profiles/${existingProvider?.name}-${Date.now()}`;
-    const url = await uploadToS3(file, key);
+    const webpBuffer = await sharp(file.buffer)
+      .resize(800, 800, {
+        fit: 'inside',
+        withoutEnlargement: true
+      })
+      .webp({
+        quality: 75, // Qualidade mais baixa para maior compressão
+        effort: 6,   // Máximo esforço de compressão
+      })
+      .toBuffer();
 
-    console.log("url",url)
-    console.log("key",key)
-    
+
+    const webpFile: Express.Multer.File = {
+      ...file,
+      buffer: webpBuffer,
+      mimetype: 'image/webp',
+      originalname: file.originalname.replace(/\.[^/.]+$/, '.webp'),
+    };
+
+    const key = `profiles/${existingProvider?.name
+      .toLowerCase()
+      .normalize('NFD')          // separa letras e acentos
+      .replace(/[\u0300-\u036f]/g, '') // remove os acentos
+      .replace(/ /g, '-')        // espaços → hífen
+      .replace(/[^a-z0-9-]/g, '') // remove outros caracteres inválidos
+      }-${Date.now()}`;
+
+    const url = await uploadToS3(webpFile, key);
+
     const updatedProvider = await this.providerRepository.update(id, {
       photoUrl: url
     });
